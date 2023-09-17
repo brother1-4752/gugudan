cdb58eb691683abfa1c4c1ab39196bee5cd1dcdb
brother1-4752 <guddlf4752@gmail.com>
2023-09-17 23:44:13 +0900

    feat: 커스텀훅 적용 / timer컴포넌트 구현

--- components/Timer/Timer.tsx
+++ components/Timer/Timer.tsx
@@ -2,24 +2,29 @@ import { useRecoilState, useRecoilValue } from 'recoil'
 import { timeState } from '@/recoil/timer/atom'
 import { stepState } from '@/recoil/step/atom'
 import { useCallback, useEffect } from 'react'
+import useInterval from '@/hooks/useInterval'
 
 export default function Timer() {
   const [time, setTime] = useRecoilState(timeState)
   const step = useRecoilValue(stepState)
 
-  const startTimer = useCallback(() => {
-    let timer = setInterval(() => {
-      setTime((prev) => prev + 0.005)
-    }, 10)
+  // const startTimer = useCallback(() => {
+  //   let timer = useInterval(() => {
+  //     setTime((prev) => prev + 0.05)
+  //   }, 10)
 
-    if (step === 20) {
-      clearInterval(timer)
-    }
-  }, [])
+  //   if (step === 20) {
+  //     clearInterval(timer)
+  //   }
+  // }, [])
 
-  useEffect(() => {
-    startTimer()
-  }, [startTimer])
+  // useEffect(() => {
+  //   startTimer()
+  // }, [startTimer])
+
+  useInterval(() => {
+    setTime((prev) => prev + 0.005)
+  }, 10)
 
   const refinedTime = time.toFixed(2)
 
--- pages/report/index.tsx
+++ pages/report/index.tsx
@@ -3,10 +3,16 @@ import { useRouter } from 'next/router'
 export default function Report() {
   const router = useRouter()
 
+  const goToHome = () => {
+    sessionStorage.removeItem('reportData')
+
+    router.push('/')
+  }
+
   return (
     <div>
       <h1>레포트</h1>
-      <button onClick={() => router.push('/')}>홈으로 가기</button>
+      <button onClick={goToHome}>홈으로 가기</button>
     </div>
   )
 }
